01d622dedb5ae0591416b4b19b06a3f2a2098050
ssoDTlab <156610340+ssoDTlab@users.noreply.github.com>
2024-05-01 06:24:58 +0000

    끝!!

--- lesson-13/main.js
+++ lesson-13/main.js
@@ -1,16 +1,50 @@
 // main.js
 "use strict";
 
+const nodemon = require("nodemon");
+
 const port = 3000,
   express = require("express"),
   layouts = require("express-ejs-layouts"),
   homeController = require("./controllers/homeController"),
   errorController = require("./controllers/errorController"),
-  app = express();
+  app = express(),
+  MongoDB = require('mongodb').MongoClient,
+  dbURL = "mongodb+srv://llfkstep9811:1h3cH8EXPIlCjnsh@ut-node.e8lneix.mongodb.net/?retryWrites=true&w=majority",
+  dbName = "ut-node";
+
   // @TODO: 몽고DB 모듈의 요청
 
-// @TODO: 로컬 MongoDB 데이터베이스 서버 연결 설정
+// @TODO: Atlas MongoDB 데이터베이스 서버 연결 설정
+MongoDB.connect(dbURL,(error,client) => {
+  if (error) throw error;
+  
+  let db = client.db(dbName);  // ut-node
+  db.collection("contacts")
+  .find()
+  .toArray((error,data) => {
+    if (error) throw error;
+    console.log(data);
+  });
 
+  db.collection("contacts")
+  .insertOne({
+    name : "Psy",
+    job : "singer",
+    from : "Korea"
+  },(error,result) => {
+    if (error) throw error;
+    console.log(result);
+  });
+})
+.then(() => {
+  console.log("DB connected!!!!!");
+})
+.catch((error) => {
+  console.log("DB connection FAILED!");
+  console.log(error);
+  process.exit(1);
+});
 
 app.set("port", process.env.PORT || port);
 app.set("view engine", "ejs");
--- lesson-14/main2.js
+++ lesson-14/main2.js
@@ -1,40 +1,90 @@
 // main2.js
 "use strict";
 
+const subscriber = require("./models/subscriber");
+
 const port = 3000,
   express = require("express"),
   layouts = require("express-ejs-layouts"),
   homeController = require("./controllers/homeController"),
   errorController = require("./controllers/errorController"),
-  // @TODO: Subscriber 모델 가져오기
+  Subscriber = require("./models/subscriber"),  
   app = express();
 
 /**
  * @TODO: Listing 14.1 (p. 205)
  * Mongoose를 사용한 MongoDB 연결
  */
+const mongoose = require("mongoose"),
+  dbURL = "mongodb+srv://llfkstep9811:1h3cH8EXPIlCjnsh@ut-node.e8lneix.mongodb.net/?retryWrites=true&w=majority",
+  dbName = "ut-node";
+mongoose.connect(
+  dbURL + "/" + dbName, // URL + dbName
+  { useNewUrlParser : true }
+);
 
-
+const db = mongoose.connection;
 /**
  * @TODO: Listing 14.2 (p. 206)
  * 데이터베이스 연결 이벤트 처리
  */
-
+db.once("open", () => {
+  console.log("DB connected!!!!");
+});
 
 /**
  * Listing 14.4 (p. 207)
  * 생성과 저장 구문
  */
+//Version1
+let sub1 = new Subscriber({
+  name : "sangwon",
+  email : "llfkstep9811@gmail.com",
+  phone : "123456789",
+  newsletter : true
+});
 
+sub1.save()
+  .then(savedDoc => {
+    console.group(savedDoc);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+// Version2
+Subscriber.create({
+    name : "Tom Hiddleston",
+    email : "loki@mv.com",
+    phone : 66666666,
+    newsletter : false
+})
+.then(savedDoc => {
+  console.group(savedDoc);
+})
+.catch(error => {
+  console.log(error);
+});
 
 // (선택) DB find() after 14.5
-
-
+let tom = Subscriber
+            .findOne({name : "Tom Hiddleston"})
+            .where(
+              "email",
+              /loki/
+            );
+console.log("Found: ", tom);
 /**
  * @TODO: Listing 14.6 (p. 208)
  * 데이터베이스에서 데이터 검색
  */
+var query = Subscriber.findOne({
+  name : "sangwon"
+}).where("email", /sangwon/);
 
+query.exec()
+    .then((error,data)=>{
+      if(data) console.log(data.name);
+    });
 
 app.set("port", process.env.PORT || port);
 app.set("view engine", "ejs");
